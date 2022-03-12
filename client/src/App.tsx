8de26515a4593b719a283e9a22ae947708b60019
Proioxis4 <lucadaniele444@gmail.com>
2022-03-12 21:19:57 +0100

    created NavBar component v1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,113 +1,22 @@
-import React, { useEffect, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
-
-function App() {
-  const [windowDimension, setWindowDimension] = useState(0);
-
-  useEffect(() => {
-    setWindowDimension(window.innerWidth);
-  }, []);
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimension(window.innerWidth);
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const isMobile = windowDimension <= 640;
-  return (
-    <Styles.Wrapper>
-      <CSSReset />
-
-      {isMobile ? (
-        <MobileNavbar.Wrapper>
-          <MobileNavbar.Items>
-            <MobileNavbar.Item>Home</MobileNavbar.Item>
-            <MobileNavbar.Item>Play</MobileNavbar.Item>
-            <MobileNavbar.Item>About</MobileNavbar.Item>
-          </MobileNavbar.Items>
-        </MobileNavbar.Wrapper>
-      ) : (
-        <Navbar.Wrapper>
-          <Navbar.Logo>Chess Opening Trainer</Navbar.Logo>
-          <Navbar.Items>
-            <Navbar.Item>Home</Navbar.Item>
-            <Navbar.Item>Play</Navbar.Item>
-            <Navbar.Item>About</Navbar.Item>
-          </Navbar.Items>
-        </Navbar.Wrapper>
-      )}
-    </Styles.Wrapper>
-  );
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import styled from "styled-components";
+function Index() {
+  return <h2>prova</h2>;
 }
 
-const Navbar = {
-  Wrapper: styled.nav`
-    flex: 1;
-
-    align-self: flex-start;
-
-    padding: 1rem 3rem;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    background-color: white;
-  `,
-  Logo: styled.h1`
-    border: 1px solid gray;
-    padding: 0.5rem 1rem;
-  `,
-  Items: styled.ul`
-    display: flex;
-    list-style: none;
-  `,
-  Item: styled.li`
-    padding: 0 1rem;
-    cursor: pointer;
-  `,
-};
-
-const CSSReset = createGlobalStyle`
-  *,
-  *::before, 
-  *::after {
-    margin: 0; 
-    padding: 0;
-    box-sizing: inherit;
-  }
-
-  html {
-    font-size: 62.5%; /*1rem = 10px*/
-    box-sizing: border-box;    
-  }  
-
-  body {
-    font-size: 1.4rem;
-    font-family: sans-serif;  
-  }
-`;
-
-const MobileNavbar = {
-  Wrapper: styled(Navbar.Wrapper)`
-    position: fixed;
-    width: 100vw;
-    bottom: 0;
-
-    justify-content: center;
-  `,
-  Items: styled(Navbar.Items)`
-    flex: 1;
-    padding: 0 2rem;
-
-    justify-content: space-around;
-  `,
-  Item: styled(Navbar.Item)``,
-};
+function App() {
+  return (
+    <>
+      <Styles.Wrapper>
+        <NavBar />
+      </Styles.Wrapper>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </>
+  );
+}
 
 const Styles = {
   Wrapper: styled.main`
@@ -116,5 +25,4 @@ const Styles = {
     height: 100vh;
   `,
 };
-
 export default App;
