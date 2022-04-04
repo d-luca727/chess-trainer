f8ea6af40ae8b93279b6a0ff491ca476e9882e13
Proioxis4 <lucadaniele444@gmail.com>
2022-04-04 20:57:57 +0200

    put post routes

--- controllers/fens.js
+++ controllers/fens.js
@@ -1,7 +1,16 @@
 const Fen = require("../models/Fen");
 const ErrorResponse = require("../utils/ErrorRes.js");
 
-exports.fens = async (req, res, next) => {
+exports.getFens = async (req, res, next) => {
+  console.log("fens!");
+  res.status(200).json({ success: true, data: "hello!!!!" });
+};
+exports.postFens = async (req, res, next) => {
+  console.log("fens!");
+  res.status(200).json({ success: true, data: "hello!!!!" });
+};
+
+exports.putFens = async (req, res, next) => {
   console.log("fens!");
   res.status(200).json({ success: true, data: "hello!!!!" });
 };
--- routes/fens.js
+++ routes/fens.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { fens } = require("../controllers/fens");
+const { getFens, postFens, PutFens } = require("../controllers/fens");
 
-router.route("/").get(fens);
+router.route("/").get(getFens);
+router.route("/").post(postFens);
+router.route("/").put(putFens);
 
 module.exports = router;
