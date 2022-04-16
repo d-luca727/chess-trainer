ea948a148d43451564f08c48d767de69af66c6be
Proioxis4 <lucadaniele444@gmail.com>
2022-04-16 19:24:58 +0200

    search bar

--- client/src/components/Collections.tsx
+++ client/src/components/Collections.tsx
@@ -4,13 +4,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Chessground from "@react-chess/chessground";
 import { Link, useNavigate } from "react-router-dom";
-import { Card, Row, Col, Button } from "antd";
+import { Card, Row, Col, Button, Input } from "antd";
 
+const { Search } = Input;
 const boardWidth = 150;
 
 const Collections = () => {
   const navigate = useNavigate();
   const [fens, setFens] = useState<any>([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     const fetch = async () => {
       const config = {
@@ -54,44 +56,61 @@ const Collections = () => {
         <Col span={24}>
           <Button onClick={() => navigate("/add-study")}>Add a Study</Button>
         </Col>
+        <Search
+          placeholder="Search by Study Name"
+          /* onSearch={onSearch} */
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchTerm(e.target.value)
+          }
+          enterButton
+        />
+        {fens
+          ?.filter((fen: any) => {
+            if (searchTerm == "") return fen;
+            else if (
+              fen.collection_name
+                .toLowerCase()
+                .includes(searchTerm.toLocaleLowerCase())
+            )
+              return fen;
+          })
+          .map(
+            (fen: {
+              _id: React.Key | null | undefined;
+              collection_name: any;
+              by: any;
+              fens: string | any[];
+            }) => (
+              <Col xs={24} sm={12} lg={6} className="fen-card" key={fen._id}>
+                <Link to={`/study/${fen._id}`}>
+                  <Card
+                    title={`${fen.collection_name}. `}
+                    extra={`by ${fen.by}. `}
+                    hoverable
+                  >
+                    <p>
+                      {fen?.fens[0] !== undefined ? (
+                        <Chessground
+                          height={boardWidth}
+                          width={boardWidth}
+                          config={{ fen: fen?.fens[0].fen, coordinates: false }}
+                        />
+                      ) : (
+                        <Chessground
+                          height={boardWidth}
+                          width={boardWidth}
+                          config={{ coordinates: false }}
+                        />
+                      )}
+                    </p>
+                    <p>Number of Positions to study: {fen.fens.length}</p>
 
-        {fens?.map(
-          (fen: {
-            _id: React.Key | null | undefined;
-            collection_name: any;
-            by: any;
-            fens: string | any[];
-          }) => (
-            <Col xs={24} sm={12} lg={6} className="fen-card" key={fen._id}>
-              <Link to={`/study/${fen._id}`}>
-                <Card
-                  title={`${fen.collection_name}. `}
-                  extra={`by ${fen.by}. `}
-                  hoverable
-                >
-                  <p>
-                    {fen?.fens[0] !== undefined ? (
-                      <Chessground
-                        height={boardWidth}
-                        width={boardWidth}
-                        config={{ fen: fen?.fens[0].fen, coordinates: false }}
-                      />
-                    ) : (
-                      <Chessground
-                        height={boardWidth}
-                        width={boardWidth}
-                        config={{ coordinates: false }}
-                      />
-                    )}
-                  </p>
-                  <p>Number of Positions to study: {fen.fens.length}</p>
-
-                  {/* <Button onClick={() => onDeleteStudy(fen._id)}}>Delete</Button> */}
-                </Card>
-              </Link>
-            </Col>
-          )
-        )}
+                    {/* <Button onClick={() => onDeleteStudy(fen._id)}}>Delete</Button> */}
+                  </Card>
+                </Link>
+              </Col>
+            )
+          )}
       </Row>
     </>
   );
