d2fd6175c116580a241a7cce868000f6b45c70cd
Proioxis4 <lucadaniele444@gmail.com>
2022-04-20 15:29:25 +0200

    Home component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,17 +1,28 @@
 import Chessground from "@react-chess/chessground";
+import { Button, Col, Row } from "antd";
 import React from "react";
-
+import { Link, useNavigate } from "react-router-dom";
+const logo = require("../imgs/logo2.jpg");
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div id="about" className="block aboutBlock">
       <div className="container-fluid">
         <br />
         <div className="titleHolder">
+          <img src={logo}></img>
           <h1>Opening Trainer</h1>
           <p>The best way to learn openings</p>
         </div>
         <div className="contentHolder">
-          <p style={{ textAlign: "center" }}></p>
+          <p style={{ textAlign: "center" }}>
+            <Button onClick={() => navigate("play")}>Start Training!</Button>
+          </p>
+          <p style={{ textAlign: "center" }}>
+            <a href="https://www.youtube.com/" target="_blank">
+              <Button>Get Started </Button>
+            </a>
+          </p>
         </div>
       </div>
     </div>
